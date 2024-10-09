d67ebe6d4eae00387f34a5a76db6e8f5f3c36bcc
Sebastian <sebassuarez103@gmail.com>
2024-10-09 18:56:01 -0500

    Inicializacion del proyecto, conexion con la base de datos, implementacion de mapper

---
+++ src/main/java/co/edu/uco/ubico/reservas/init/UbicoReservasMsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ubico.reservas.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicoReservasMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbicoReservasMsApplication.class, args);
+	}
+
+}
